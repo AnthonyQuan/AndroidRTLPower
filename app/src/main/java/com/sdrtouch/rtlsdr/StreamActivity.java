f2a3546e6bce2333fc030d1c5bbfddcafb92cd76
AnthonyQuan <11380173@student.uts.edu.au>
2016-10-29 22:29:20 +1100

    Modifed launch screen with run button to get params

--- app/src/main/java/com/sdrtouch/rtlsdr/StreamActivity.java
+++ app/src/main/java/com/sdrtouch/rtlsdr/StreamActivity.java
@@ -52,201 +52,67 @@ import marto.rtl_tcp_andro.R;
 
 public class StreamActivity extends FragmentActivity implements Log.Callback {
 
-	private TextView terminal;
-	private ScrollView scroll;
-	private EditText arguments;
-	private ToggleButton onoff;
-
 	private static final int START_REQ_CODE = 1;
 	
 	private BinaryRunnerService service;
-	private final ServiceConnection mConnection = new ServiceConnection() {
-
-		@Override
-		public void onServiceConnected(ComponentName name, IBinder ibinder) {
-			LocalBinder binder = (LocalBinder) ibinder;
-			binder.registerCallback(serviceStatusCallback);
-			service = binder.getService();
-		}
-
-		@Override
-		public void onServiceDisconnected(ComponentName name) {
-			runOnUiThread(new Runnable() {
-				@Override
-				public void run() {
-					onoff.setChecked(false);
-				}
-			});
-			service = null;
-		}
-    };
-    
-    private final BinaryRunnerService.StatusCallback serviceStatusCallback = new BinaryRunnerService.StatusCallback() {
-		@Override
-		public void onServerRunning() {
-			runOnUiThread(new Runnable() {
-				@Override
-				public void run() {
-					onoff.setChecked(true);
-				}
-			});
-		}
-		
-		@Override
-		public void onServerNotRunning() {
-			runOnUiThread(new Runnable() {
-				@Override
-				public void run() {
-					onoff.setChecked(false);
-				}
-			});
-		}
-	};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_stream);
 
-		terminal = (TextView) findViewById(R.id.terminal);
-		scroll = (ScrollView) findViewById(R.id.ScrollArea);
-		arguments = (EditText) findViewById(R.id.commandline);
-		
-		terminal.setText(Log.getFullLog());
-		
-		findViewById(R.id.enable_gui).setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				StreamActivity.this.findViewById(R.id.statusmsg).setVisibility(View.GONE);
-				StreamActivity.this.findViewById(R.id.gui).setVisibility(View.VISIBLE);
-			}
-		});
 
-		
-		(onoff = (ToggleButton) findViewById(R.id.onoff)).setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				Check.isNotNull(service);
-				if (service.isRunning()) {
-					service.closeService();
-				} else {
-					Intent intent = new Intent(Intent.ACTION_VIEW).setData(Uri.parse("iqsrc://" + arguments.getText().toString()));
-					intent.setClass(StreamActivity.this, DeviceOpenActivity.class);
-					StreamActivity.this.startActivityForResult(intent, START_REQ_CODE);
-				}
-				onoff.setChecked(service.isRunning());
-			}
-		});
-
-		findViewById(R.id.license).setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View ignored) {
-				showDialog(dialogs.DIAG_LICENSE);
-			}
-		});
-
-		findViewById(R.id.copybutton).setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View ignored) {
-				final String textToClip = terminal.getText().toString();
-				ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
-				ClipData clip = ClipData.newPlainText("text label",textToClip);
-				clipboard.setPrimaryClip(clip);
-				Toast.makeText(getApplicationContext(), R.string.copied_to_clip, Toast.LENGTH_LONG).show();
-			}
-		});
-		
-		findViewById(R.id.clearbutton).setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				Log.clear();
-			}
-		});
-		
-		findViewById(R.id.help).setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				StreamActivity.this.showDialog(dialogs.DIAG_ABOUT);
-			}
-		});
-
-		if (!RtlSdrApplication.IS_PLATFORM_SUPPORTED) {
-			((TextView) findViewById(R.id.warntext)).setText(R.string.platform_not_supported);
-		}
 	}
+
+	public void RunButtonOnClick(View view)
+    {
+        EditText freqLower = (EditText) findViewById(R.id.freqLower);
+        EditText freqHigher = (EditText) findViewById(R.id.freqHigher);
+        EditText bins = (EditText) findViewById(R.id.bins);
+        EditText fileName = (EditText) findViewById(R.id.fileName);
+        TextView textView = (TextView) findViewById(R.id.textView);
+
+        textView.append("Lower Frequency Bound: " + freqLower.getText().toString() + "\n");
+        textView.append("Higher Frequency Bound: " + freqHigher.getText().toString() + "\n");
+        textView.append("Number of Bins: " + bins.getText().toString() + "\n");
+        textView.append("File Name: " + fileName.getText().toString());
+
+    }
 
 	public void showDialog(final DialogManager.dialogs id, final String ... args) {
 
-		final FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-		final Fragment prev = getSupportFragmentManager().findFragmentByTag("dialog");
-		if (prev != null) {
-			ft.remove(prev);
-		}
-		ft.addToBackStack(null);
-
-		// Create and show the dialog.
-		final DialogFragment newFragment = DialogManager.invokeDialog(id, args);
-		try {
-			newFragment.show(ft, "dialog");
-		} catch (Throwable t) {t.printStackTrace();}
 	}
 	
 	@Override
 	protected void onSaveInstanceState(Bundle outState) {
-		outState.putString("args", arguments.getText().toString());
-		super.onSaveInstanceState(outState);
+
 	}
 	
 	@Override
 	protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
 		super.onRestoreInstanceState(savedInstanceState);
-		arguments.setText(savedInstanceState.getString("args"));
+
 	}
 	
 	@Override
 	protected void onResume() {
 		super.onResume();
-		terminal.setText(Log.getFullLog());
-		bindService(new Intent(this, BinaryRunnerService.class), mConnection, Context.BIND_AUTO_CREATE);
-		Log.registerCallback(this);
+
 	}
 	
 	@Override
 	protected void onPause() {
 		super.onPause();
-		unbindService(mConnection);
-		Log.unregisterCallback(this);
+
 	}
 
 	@Override
 	protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
-		runOnUiThread(new Runnable() {
-			@Override
-			public void run() {
-				if (requestCode == START_REQ_CODE) {
-					if (resultCode == RESULT_OK)
-						Log.appendLine("Starting was successful!");
-					else {
-						err_info einfo = err_info.unknown_error;
-						try {
-							einfo = err_info.values()[data.getIntExtra("marto.rtl_tcp_andro.RtlTcpExceptionId", err_info.unknown_error.ordinal())];
-						} catch (Throwable ignored) {
-						}
-						Log.appendLine("ERROR STARTING! Reason: " + einfo);
-					}
-				}
-			}
-		});
+
 	}
 	
 	@Override
 	public void onChanged() {
-		runOnUiThread(new Runnable() {
-			@Override
-			public void run() {
-				terminal.setText(Log.getFullLog());
-				scroll.pageScroll(ScrollView.FOCUS_DOWN);
-			}
-		});
+
 	}
 }
