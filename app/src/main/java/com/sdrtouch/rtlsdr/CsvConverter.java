d5ca2239fa10cb6a4aec9c90844a790b18363e1a
Jackie-C <Jackie.Cheung@student.uts.edu.au>
2017-04-29 14:55:48 +1000

    Fixed CsvConverter error after updating frequency range. Added CsvConverter comments

--- app/src/main/java/com/sdrtouch/rtlsdr/CsvConverter.java
+++ app/src/main/java/com/sdrtouch/rtlsdr/CsvConverter.java
@@ -58,28 +58,33 @@ public class CsvConverter extends AsyncTask<String, Void, Void> {
     }
 
     private void convert(String dirName, String batchID, float altitude, double latitude, double longitude, String integrationInterval) throws IOException, ParseException {
+        // Get CSV file location
         String csvFile = findFile(dirName, batchID);
 
-        if (!csvFile.equals("NOTFOUND")) { // only proceed when there is a valid csv file
+        // Only proceed when there is a valid csv file
+        if (!csvFile.equals("NOTFOUND")) {
             Log.d("RTL_LOG", "Found .csv file, starting conversion...");
             BufferedReader br = new BufferedReader(new FileReader(csvFile));
+            // One entry in the HashMap for each spectrum sweep
             Map<String, String> integrationsList = new HashMap<>();
             String line;
             String csvSplitBy = ", ";
             boolean integrationExists;
 
-            //Iterate through entire CSV file
+            // Iterate through CSV one row at a time
             while ((line = br.readLine()) != null) {
-                String[] entry = line.split(csvSplitBy); //Use comma-space as separator
+                // Populate string array entry, based on the value of each column in the row
+                String[] entry = line.split(csvSplitBy);
                 integrationExists = false;
 
-                //Convert datetime to unix timestamp
+                //Convert first 2 columns into a to unix timestamp, to be used as a key
 				String datetime = entry[0] + entry[1];
                 SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-ddHH:mm:ss");
                 long unixTime =  sdf.parse(datetime).getTime() / 1000;
                 String unix = String.valueOf(unixTime);
 
-                //Set boolean if a key already exists in the HashMap
+                // Iterate though integration HashMap and
+                // set boolean if a key (unix) already exists in the HashMap
                 for (String key : integrationsList.keySet()) {
                     if (key.equals(unix)) {
                         integrationExists = true;
@@ -90,77 +95,59 @@ public class CsvConverter extends AsyncTask<String, Void, Void> {
                 //If a key already exists in the HashMap
                 if (integrationExists) {
                     String json;
+                    // Retrieve existing JSON for the same integration
                     json = integrationsList.get(unix);
+                    // Parse values from CSV
                     int frequencyLow = Integer.parseInt(entry[2]);
                     int frequencyStep = (int)Double.parseDouble(entry[4]);
-
+                    // Translate values into JSON format
                     String valuesHeader = "{\"frequencyLow\":\"" + frequencyLow + "\",\"frequencyHigh\":\"" + entry[3] + "\",\"frequencyStep\":\"" + frequencyStep + "\",\"metricValues\":[";
-                    String valuesBody = "{\"" + Integer.toString(frequencyLow) + "\":" + entry[6] + "}," +  "{\"" + Integer.toString(frequencyLow + (frequencyStep)) + "\":" + entry[7] + "}," +
-                            "{\"" + Integer.toString(frequencyLow + (frequencyStep * 2)) + "\":" + entry[8] + "}," + "{\"" + Integer.toString(frequencyLow + (frequencyStep * 3)) + "\":" + entry[9] + "}," +
-                            "{\"" + Integer.toString(frequencyLow + (frequencyStep * 4)) + "\":" + entry[10] + "}," + "{\"" + Integer.toString(frequencyLow + (frequencyStep * 5)) + "\":" + entry[11] + "}," +
-                            "{\"" + Integer.toString(frequencyLow + (frequencyStep * 6)) + "\":" + entry[12] + "}," + "{\"" + Integer.toString(frequencyLow + (frequencyStep * 7)) + "\":" + entry[13] + "}," +
-                            "{\"" + Integer.toString(frequencyLow + (frequencyStep * 8)) + "\":" + entry[14] + "}," + "{\"" + Integer.toString(frequencyLow + (frequencyStep * 9)) + "\":" + entry[15] + "}," +
-                            "{\"" + Integer.toString(frequencyLow + (frequencyStep * 10)) + "\":" + entry[16] + "}," + "{\"" + Integer.toString(frequencyLow + (frequencyStep * 11)) + "\":" + entry[17] + "}," +
-                            "{\"" + Integer.toString(frequencyLow + (frequencyStep * 12)) + "\":" + entry[18] + "}," + "{\"" + Integer.toString(frequencyLow + (frequencyStep * 13)) + "\":" + entry[19] + "}," +
-                            "{\"" + Integer.toString(frequencyLow + (frequencyStep * 14)) + "\":" + entry[20] + "}," + "{\"" + Integer.toString(frequencyLow + (frequencyStep * 15)) + "\":" + entry[21] + "}," +
-                            "{\"" + Integer.toString(frequencyLow + (frequencyStep * 16)) + "\":" + entry[22] + "}," + "{\"" + Integer.toString(frequencyLow + (frequencyStep * 17)) + "\":" + entry[23] + "}," +
-                            "{\"" + Integer.toString(frequencyLow + (frequencyStep * 18)) + "\":" + entry[24] + "}," + "{\"" + Integer.toString(frequencyLow + (frequencyStep * 29)) + "\":" + entry[25] + "}," +
-                            "{\"" + Integer.toString(frequencyLow + (frequencyStep * 20)) + "\":" + entry[26] + "}," + "{\"" + Integer.toString(frequencyLow + (frequencyStep * 21)) + "\":" + entry[27] + "}," +
-                            "{\"" + Integer.toString(frequencyLow + (frequencyStep * 22)) + "\":" + entry[28] + "}," + "{\"" + Integer.toString(frequencyLow + (frequencyStep * 23)) + "\":" + entry[29] + "}," +
-                            "{\"" + Integer.toString(frequencyLow + (frequencyStep * 24)) + "\":" + entry[30] + "}," + "{\"" + Integer.toString(frequencyLow + (frequencyStep * 25)) + "\":" + entry[31] + "}," +
-                            "{\"" + Integer.toString(frequencyLow + (frequencyStep * 26)) + "\":" + entry[32] + "}," + "{\"" + Integer.toString(frequencyLow + (frequencyStep * 27)) + "\":" + entry[33] + "}," +
-                            "{\"" + Integer.toString(frequencyLow + (frequencyStep * 28)) + "\":" + entry[34] + "}," + "{\"" + Integer.toString(frequencyLow + (frequencyStep * 29)) + "\":" + entry[35] + "}," +
-                            "{\"" + Integer.toString(frequencyLow + (frequencyStep * 30)) + "\":" + entry[36] + "}," + "{\"" + Integer.toString(frequencyLow + (frequencyStep * 31)) + "\":" + entry[37] + "}," +
-                            "{\"" + Integer.toString(frequencyLow + (frequencyStep * 32)) + "\":" + entry[38] + "}]}";
-
+                    String valuesBody = "{\"" + Integer.toString(frequencyLow) + "\":" + entry[6] + "}]}";
+                    // Append new JSON value to existing JSON
                     json = json + "," + valuesHeader + valuesBody;
+                    // Save appended JSON to HashMap
                     integrationsList.put(unix, json);
                 }
-                //Create a new HashMap entry for this key
+                // Else loop for when an integration does not exist in the HashMap
+                // Create a new HashMap entry for this key
+                // Populate HashMap with JSON containing integration header
                 else {
+                    // Parse values from CSV
                     int frequencyLow = Integer.parseInt(entry[2]);
                     int frequencyStep = (int)Double.parseDouble(entry[4]);
-
+                    // Translate values into JSON format
                     String integrationHeader = "{\"unixTimestamp\":\"" + unixTime + "\",\"date\":\"" + entry[0] + "\",\"time\":\"" + entry[1] + "\",\"totalSamples\":\"" + entry[5] + "\",\"metricSeries\":[";
                     String valuesHeader = "{\"frequencyLow\":\"" + frequencyLow + "\",\"frequencyHigh\":\"" + entry[3] + "\",\"frequencyStep\":\"" + frequencyStep + "\",\"metricValues\":[";
-                    String valuesBody = "{\"" + Integer.toString(frequencyLow) + "\":" + entry[6] + "}," +  "{\"" + Integer.toString(frequencyLow + (frequencyStep)) + "\":" + entry[7] + "}," +
-                            "{\"" + Integer.toString(frequencyLow + (frequencyStep * 2)) + "\":" + entry[8] + "}," + "{\"" + Integer.toString(frequencyLow + (frequencyStep * 3)) + "\":" + entry[9] + "}," +
-                            "{\"" + Integer.toString(frequencyLow + (frequencyStep * 4)) + "\":" + entry[10] + "}," + "{\"" + Integer.toString(frequencyLow + (frequencyStep * 5)) + "\":" + entry[11] + "}," +
-                            "{\"" + Integer.toString(frequencyLow + (frequencyStep * 6)) + "\":" + entry[12] + "}," + "{\"" + Integer.toString(frequencyLow + (frequencyStep * 7)) + "\":" + entry[13] + "}," +
-                            "{\"" + Integer.toString(frequencyLow + (frequencyStep * 8)) + "\":" + entry[14] + "}," + "{\"" + Integer.toString(frequencyLow + (frequencyStep * 9)) + "\":" + entry[15] + "}," +
-                            "{\"" + Integer.toString(frequencyLow + (frequencyStep * 10)) + "\":" + entry[16] + "}," + "{\"" + Integer.toString(frequencyLow + (frequencyStep * 11)) + "\":" + entry[17] + "}," +
-                            "{\"" + Integer.toString(frequencyLow + (frequencyStep * 12)) + "\":" + entry[18] + "}," + "{\"" + Integer.toString(frequencyLow + (frequencyStep * 13)) + "\":" + entry[19] + "}," +
-                            "{\"" + Integer.toString(frequencyLow + (frequencyStep * 14)) + "\":" + entry[20] + "}," + "{\"" + Integer.toString(frequencyLow + (frequencyStep * 15)) + "\":" + entry[21] + "}," +
-                            "{\"" + Integer.toString(frequencyLow + (frequencyStep * 16)) + "\":" + entry[22] + "}," + "{\"" + Integer.toString(frequencyLow + (frequencyStep * 17)) + "\":" + entry[23] + "}," +
-                            "{\"" + Integer.toString(frequencyLow + (frequencyStep * 18)) + "\":" + entry[24] + "}," + "{\"" + Integer.toString(frequencyLow + (frequencyStep * 29)) + "\":" + entry[25] + "}," +
-                            "{\"" + Integer.toString(frequencyLow + (frequencyStep * 20)) + "\":" + entry[26] + "}," + "{\"" + Integer.toString(frequencyLow + (frequencyStep * 21)) + "\":" + entry[27] + "}," +
-                            "{\"" + Integer.toString(frequencyLow + (frequencyStep * 22)) + "\":" + entry[28] + "}," + "{\"" + Integer.toString(frequencyLow + (frequencyStep * 23)) + "\":" + entry[29] + "}," +
-                            "{\"" + Integer.toString(frequencyLow + (frequencyStep * 24)) + "\":" + entry[30] + "}," + "{\"" + Integer.toString(frequencyLow + (frequencyStep * 25)) + "\":" + entry[31] + "}," +
-                            "{\"" + Integer.toString(frequencyLow + (frequencyStep * 26)) + "\":" + entry[32] + "}," + "{\"" + Integer.toString(frequencyLow + (frequencyStep * 27)) + "\":" + entry[33] + "}," +
-                            "{\"" + Integer.toString(frequencyLow + (frequencyStep * 28)) + "\":" + entry[34] + "}," + "{\"" + Integer.toString(frequencyLow + (frequencyStep * 29)) + "\":" + entry[35] + "}," +
-                            "{\"" + Integer.toString(frequencyLow + (frequencyStep * 30)) + "\":" + entry[36] + "}," + "{\"" + Integer.toString(frequencyLow + (frequencyStep * 31)) + "\":" + entry[37] + "}," +
-                            "{\"" + Integer.toString(frequencyLow + (frequencyStep * 32)) + "\":" + entry[38] + "}]}";
+                    String valuesBody = "{\"" + Integer.toString(frequencyLow) + "\":" + entry[6] + "}]}";
+                    // Build JSON string
                     String integrationJSON = integrationHeader + valuesHeader + valuesBody;
+                    // Save JSON to HashMap
                     integrationsList.put(unix, integrationJSON);
                 }
             }
 
+            // CSV has finished iterating, begin building JSON output file...
             StringBuilder sb = new StringBuilder();
-            SortedSet<String> keys = new TreeSet<>(integrationsList.keySet()); //Sort HashMap by key (unix timestamp)
+            //Sort HashMap by key (unix timestamp), earliest to latest
+            SortedSet<String> keys = new TreeSet<>(integrationsList.keySet());
 
             //Iterate through HashMap to build JSON
             for (String key : keys) {
-                sb.append(integrationsList.get(key)); //Build JSON Body
-                sb.append("]},"); //Close off JSON Array and JSON Object
+                //Build JSON Body
+                sb.append(integrationsList.get(key));
+                //Split or close each integration using below seperator
+                sb.append("]},");
             }
 
-            //Add JSON header fields
+            //Build JSON header
             String jsonHeader = "{\"BATCH_ID\":\"" + batchID + "\",\"altitude\":\"" + altitude + "\",\"latitude\":\"" + latitude +
                     "\",\"longitude\":\"" + longitude + "\",\"integrationInterval\":\"" + integrationInterval + "\",\"integrations\":[";
-            sb.setLength(sb.length() -1); //Remove final comma as there are no more JSON objects to be added
-            sb.append("]}"); //Close off JSON Array and JSON Object
+            //Remove final comma as there are no more JSON objects to be added
+            sb.setLength(sb.length() -1);
+            //Close off JSON Array and JSON Object
+            sb.append("]}");
 
-            //Export JSON
+            //Export JSON to the .json file
             FileWriter fr = new FileWriter(dirName + "/" + batchID + ".json");
             fr.write(jsonHeader + sb.toString());
             Log.d("RTL_LOG", "File exported to" + dirName + "/" + batchID + ".json");
